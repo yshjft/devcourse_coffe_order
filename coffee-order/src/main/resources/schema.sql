ea66d93cee22a63a5d698718649e5e36f82e223c
hong yuseok <jericoh@naver.com>
2022-05-03 18:49:39 +0900

    backend/fix: 관리자 화면 수정

--- coffee-order/src/main/resources/schema.sql
+++ coffee-order/src/main/resources/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE categories
 CREATE TABLE products
 (
     product_id   BINARY(16) PRIMARY KEY,
-    product_name VARCHAR(20) NOT NULL,
+    product_name VARCHAR(50) NOT NULL,
     category     VARCHAR(50) NOT NULL,
     price        bigint      NOT NULL,
     description  VARCHAR(500) DEFAULT NULL,
